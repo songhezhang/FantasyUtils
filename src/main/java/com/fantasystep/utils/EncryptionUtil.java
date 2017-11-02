aa480b2c9b3de4cffc1f7e61925578be445b58b4
Songhe Zhang <pppp83zsh@gmail.com>
2017-11-02 16:45:46 +0100

    Init

---
+++ src/main/java/com/fantasystep/utils/EncryptionUtil.java
@@ -0,0 +1,53 @@
+package com.fantasystep.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import sun.misc.BASE64Encoder;
+
+public class EncryptionUtil {
+
+	public final static String SHA1_PREFIX = "{SHA1}";
+	public final static String MD5_PREFIX = "{MD5}";
+
+	public static Object aesEncrypt(String password) {
+		return digest("MD5", password);
+	}
+
+	public static String digest(String algorithm, String password) {
+		if(password.startsWith(MD5_PREFIX) || password.startsWith(SHA1_PREFIX))
+			return password;
+		String r = null;
+		byte[] b = null;
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance(algorithm);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return password;
+		}
+		BASE64Encoder encoder;
+
+		md.update(password.getBytes());
+		b = md.digest();
+
+		encoder = new BASE64Encoder();
+
+		System.out.println(password + "     " + encoder.encode(b));
+
+		r = encoder.encode(b);
+
+		return "{" + algorithm + "}" + r;
+	}
+
+	public static boolean testPass(String password, String encrypted) {
+		if (password == null)
+			return false;
+		if (encrypted.toString().startsWith(MD5_PREFIX))
+			return digest("MD5", password).equals(encrypted);
+		else if (encrypted.toString().startsWith(SHA1_PREFIX))
+			return digest("SHA1", password).equals(encrypted);
+		else
+			return password.equals(encrypted);
+	}
+}
---
+++ src/main/java/com/fantasystep/utils/ValidationUtil.java
@@ -0,0 +1,16 @@
+package com.fantasystep.utils;
+
+
+import com.fantasystep.domain.Node;
+import com.fantasystep.helper.Validation;
+
+public class ValidationUtil {
+
+	public static void validate(Node node) {
+		
+	}
+
+	public static String getValidateRegex(Validation alphanumeric) {
+		return null;
+	}
+}
